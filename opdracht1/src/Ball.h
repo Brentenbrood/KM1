c5a05bacab8c580b51f8d2cd695e437b49884b9a
Brent Minkes <brentminkes@hotmail.com>
2017-09-10 18:25:02 +0200

    multiple balls

--- opdracht1/src/Ball.h
+++ opdracht1/src/Ball.h
@@ -1,5 +1,4 @@
 #pragma once
-#define _BALL // by using this if statement you prevent the class to be called more than once which would confuse the compiler
 #include "ofMain.h" // we need to include this to have a reference to the openFrameworks framework
 
 class Ball
